1c841f86fe08d174366ed5d289ea77123f8fbfe8
LeweLotki <stefanczyk@gmail.com>
2024-06-16 14:15:00 +0200

    fix

--- src/chat_gpt/prompter.py
+++ src/chat_gpt/prompter.py
@@ -18,16 +18,31 @@ class Prompter():
         self.chat_gpt = ChatGPT(api_key=api_key, model=model)
 
     def get_descriptions(self):
-        prompt = "Napisz krótki opis(dwa zdania) o czym jest poniższa piosenka: " 
+        prompt_base = "Napisz krótki opis(dwa zdania) o czym jest poniższa piosenka. Zwróć uwagę na konkretne zabiegi stylistyczne i narrację: " 
         
         songs = session.query(TableSongs).all()
+
+        matching_names = session.query(TableSongs.name)\
+            .join(TableDescription, TableSongs.name == TableDescription.name)\
+            .all()
+
         for song in songs:
-            
-            prompt += song.content
+           
+            if song.name in matching_names:
+                continue
+            print(song.name)
+            print()
+            prompt = prompt_base + song.content
             response = self.chat_gpt.get_response(prompt)
             print(response)
+            new_description = TableDescription(
+                name=song.name,
+                description=response,
+                content=song.content
+            )
+            session.add(new_description)
+            session.commit()
 
-        return
 
 
 
